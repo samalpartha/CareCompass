8b7da2d137d76d5fa374c2aeb8aa5ea6f3001021
Partha Samal <samalpartha@gmail.com>
2025-11-28 05:52:39 +0000

    fix error

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,50 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+<<<<<<< HEAD
     // Enable static HTML export for GitHub Pages
   output: 'export',
   // Disable image optimization for static export
   images: {
     unoptimized: true,
   },
+=======
+  output: 'export',
+  basePath: '/CareCompass',
+>>>>>>> a66ac95 (can u make it deployable to github pages)
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+<<<<<<< HEAD
 
+=======
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+>>>>>>> a66ac95 (can u make it deployable to github pages)
 };
 
 export default nextConfig;
